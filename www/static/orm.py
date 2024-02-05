b75d61f3486db48a4fd4b85ce37a3327e382d1f6
MB <790220273@qq.com>
2024-02-05 15:19:53 +0800

    update orm.py V1

--- www/static/orm.py
+++ www/static/orm.py
@@ -2,7 +2,7 @@
 Author: MB 790220273@qq.com
 Date: 2024-02-04 17:30:26
 LastEditors: MB 790220273@qq.com
-LastEditTime: 2024-02-04 18:15:49
+LastEditTime: 2024-02-05 15:17:47
 FilePath: \awesome-python3-webapp\www\static\orm.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -49,8 +49,79 @@ async def select(sql, args, size=None):
         return rs
         
 # Insert Update Delete
-async def execute(ql, args):
-    
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
 
-             
+# 生成占位符字符串
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+# Field类
+class Field(object):
+    
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+        
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+    
+class StringField(Field):
+    
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+        
+class BooleanField(Field):
+    
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+        
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)    
+        
+# ModelMetaclass类
+class ModelMetaclass(type):
+    
+    def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                
     
